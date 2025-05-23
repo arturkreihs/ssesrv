0c7a3d1c040eed410bf66775f5228f0a4367c605
Artur Kreihs <akreihs@ascs.pl>
2025-05-23 10:00:05 +0200

    . example in main.rs simplified;

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ use ssesrv::Server;
 
 #[tokio::main]
 async fn main() {
-    let server = Arc::new(Server::new());
-
-    // Clone server for the HTTP server task
-    let server_for_http = server.clone();
+    let sse = Arc::new(Server::new());
 
     // Spawn the warp server
-    tokio::spawn(async move {
-        server_for_http.run(3030).await;
-    });
+    {
+        let sse = sse.clone();
+        tokio::spawn(async move {
+            sse.run(3030).await;
+        });
+    }
 
     // Example: emit messages every 5 seconds
     loop {
-        server.emit("Hello SSE clients!").await;
+        sse.emit("Hello SSE clients!").await;
         tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
     }
 }
